79544c28ef8e74dbac02eee1cc3d0f221d821eb2
DevEliasKh <20316165+DevEliasKh@users.noreply.github.com>
2024-06-04 17:22:51 +0330

    ✏️ - add images, change data and make plant card

--- ui/component/plantArea.tsx
+++ ui/component/plantArea.tsx
@@ -1,13 +1,72 @@
 "use client";
 
-import { Box } from "@mui/material";
+import { Box, Card, Grid, Typography } from "@mui/material";
+
 import data from "@/data/plants.json";
+
+type plantType = {
+   id: number;
+   name: string;
+   imgRcs: string;
+};
 
 export default function PlantArea() {
    console.log(data);
-   return <div>plant area</div>;
+   return (
+      <Grid container spacing={2}>
+         <Grid item xs={12}>
+            <Typography
+               sx={{
+                  fontWeight: "500",
+                  fontSize: "1.5rem",
+               }}
+            >
+               گیاهان آپارتمانی را بهتر بشناسیم
+            </Typography>
+         </Grid>
+         {data.map((item) => (
+            <Grid item key={item.id} xs={12} sm={6}>
+               <PlantCard {...item} />
+            </Grid>
+         ))}
+      </Grid>
+   );
 }
 
-function PlantCard() {
-   return <Box></Box>;
+function PlantCard(plant: plantType) {
+   return (
+      <Card
+         sx={{
+            display: "flex",
+            alignItems: "center",
+         }}
+      >
+         <Box
+            component="img"
+            src={plant.imgRcs}
+            alt={plant.name}
+            sx={{
+               width: "20%",
+            }}
+         ></Box>
+         <Box
+            sx={{
+               display: "flex",
+               flexDirection: "column",
+               width: "100%",
+               p: 2,
+            }}
+         >
+            <Typography>{plant.name}</Typography>
+            <Box
+               component="a"
+               sx={{
+                  alignSelf: "flex-end",
+               }}
+            >
+               ادامه مطلب
+            </Box>
+         </Box>
+      </Card>
+   );
 }
