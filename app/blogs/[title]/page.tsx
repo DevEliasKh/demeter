6d422d28d73b2ad6c181206e876bf8bfe1657705
DevEliasKh <20316165+DevEliasKh@users.noreply.github.com>
2024-06-07 16:43:54 +0330

    ✏️ - display every blog page

--- app/blogs/[title]/page.tsx
+++ app/blogs/[title]/page.tsx
@@ -1,11 +1,28 @@
-"use client";
+import { Box, Typography } from "@mui/material";
 
-// TODO: import blog data, filter it , display it
+import data from "@/data/blogs.json";
 
-import { usePathname } from "next/navigation";
+// TODO: fix not displaying image
 
 export default function Page(title: any) {
-   const pathname = usePathname();
-   console.log(title.params.title);
-   return <div>page</div>;
+   const currentBlog = data.filter((blog) => blog.id == title.params.title)[0];
+
+   return (
+      <Box>
+         <Box
+            component="img"
+            src={currentBlog.imgRcs}
+            alt={currentBlog.name}
+         ></Box>
+         <Typography
+            sx={{
+               fontSize: "1.3rem",
+               fontWeight: "600",
+            }}
+         >
+            {currentBlog.name}
+         </Typography>
+         <Typography>{currentBlog.summary}</Typography>
+      </Box>
+   );
 }
