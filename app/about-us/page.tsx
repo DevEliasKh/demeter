bd2b14b5db00d4c1242ecc23ffb96ddeb8d6a754
DevEliasKh <20316165+DevEliasKh@users.noreply.github.com>
2024-06-07 18:35:17 +0330

    ✏️ - add about us page

---
+++ app/about-us/page.tsx
@@ -0,0 +1,27 @@
+import { Box, Typography } from "@mui/material";
+
+// TODO: add more details
+
+export default function Page() {
+   return (
+      <Box
+         sx={{
+            display: "flex",
+            flexDirection: "row-reverse",
+            justifyContent: "space-between",
+            gap: "1rem",
+            alignItems: "center",
+         }}
+      >
+         <Box
+            component="img"
+            src="img/data-blog/01.jpg"
+            sx={{ maxWidth: "50vw" }}
+         ></Box>
+         <Typography>
+            ما در اینجا با گردآوری برترین مقالات و آموزش های روز دنیا به شما در
+            نگه داری و پروش گل و گیاه در خانه کمک می کتیم
+         </Typography>
+      </Box>
+   );
+}
